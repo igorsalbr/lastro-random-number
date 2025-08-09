fb7225b186cd25b422a5b903a3fc0bc5b227702f
Igor Schroter Salviatto <igorschsal@gmail.com>
2025-08-09 08:07:40 -0300

    lais name

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,25 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Sorteador de Números - sorteio.com',
-  description: 'Sorteie números aleatórios de forma rápida e fácil. Personalize intervalos, quantidade e opções avançadas.',
-  keywords: 'sorteio, números, aleatório, sorteador, loteria',
-    generator: 'v0.dev'
-}
+  title: "Sorteador de Números - Lais",
+  description:
+    "Sorteie números aleatórios de forma rápida e fácil. Personalize intervalos, quantidade e opções avançadas.",
+  keywords: "sorteio, números, aleatório, sorteador, lais",
+  generator: "igorsalbr"
+};
 
 export default function RootLayout({
-  children,
+  children
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="pt-BR">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
